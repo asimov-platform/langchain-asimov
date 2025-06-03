1c6aae58d1df3b26daa003e164628c55d342a64a
Arto Bendiken <arto@asimov.so>
2025-06-03 08:57:01 +0400

    Implement the document loader.

--- langchain_asimov/document_loaders.py
+++ langchain_asimov/document_loaders.py
@@ -1,11 +1,25 @@
+# This is free and unencumbered software released into the public domain.
+
 """ASIMOV document loader."""
 
+from __future__ import annotations # for Python 3.9
+
+import json
 import logging
+import subprocess
+from .errors import AsimovModuleNotFound
 from langchain_core.document_loaders.base import BaseLoader
 from langchain_core.documents import Document
-from typing import Any, Iterator, Optional, override
+from pyld import jsonld
+from typing import Any, Iterator, cast, override
 
 logger = logging.getLogger(__file__)
+
+JSONLD_CONTEXT = {
+    "@version": 1.1,
+    "know": "https://know.dev/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+}
 
 class AsimovLoader(BaseLoader):
     """
@@ -26,10 +40,38 @@ class AsimovLoader(BaseLoader):
                 url="https://duckduckgo.com/?q=Isaac+Asimov"
             )
     """
-    def __init__(self, module: str, url: Optional[str] = None, **kwargs: Any) -> None:
+    def __init__(self, module: str, url: str, **kwargs: Any) -> None:
         self.module = module
         self.url = url
 
     @override
     def lazy_load(self) -> Iterator[Document]:
-        yield Document(page_content="", metadata={}) # TODO
+        try:
+            result = subprocess.run(
+                [f"asimov-{self.module}-importer", self.url],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            result.check_returncode()
+            output = json.loads(result.stdout)
+            output = cast(dict, jsonld.flatten(output, JSONLD_CONTEXT))
+            for resource in output["@graph"]:
+                resource_id = resource["@id"]
+                page_content = describe(resource)
+                yield Document(page_content, id=resource_id, metadata=resource)
+        except FileNotFoundError as error:
+            #logger.exception(error)
+            raise AsimovModuleNotFound(self.module) from (error if __debug__ else None)
+        except subprocess.CalledProcessError as error:
+            #logger.exception(error)
+            raise error # TODO
+        except json.decoder.JSONDecodeError as error:
+            #logger.exception(error)
+            raise error # TODO
+        except jsonld.JsonLdError as error:
+            #logger.exception(error)
+            raise error # TODO
+
+def describe(resource: dict) -> str:
+    return resource["know:summary"]["@value"] # TODO
